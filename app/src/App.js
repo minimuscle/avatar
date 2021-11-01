8d8c8e1e4cb5f0aaf867f71ac0a8d4da574ddc05
minimuscle <joshthiele@live.com.au>
2021-11-01 20:59:20 +1100

    basic setup

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <h1>Avatar Quiz</h1>
+        <hr />
+        <button classname="option">Which Character Are You?</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
